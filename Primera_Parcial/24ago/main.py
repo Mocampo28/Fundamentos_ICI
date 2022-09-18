cf4c54371485cc0b33ab5f42680a3160d05e552b
Marco Antonio Ocampo <55845927+Mocampo28@users.noreply.github.com>
2022-09-18 18:44:15 -0500

    Update main.py

--- Primera_Parcial/24ago/main.py
+++ Primera_Parcial/24ago/main.py
@@ -32,3 +32,21 @@ if __name__ == '__main__':
     print("Ingresa el año de tu nacimiento")
     aon = input()
     print("Hola" + nom + "Tienes" + añoNacimiento(aoa,aon) + "años")
+    
+    
+def funcion3(a: int, b: int, c: str) -> str:  # Asignar como valores de entrada a y b cuyo tipo de dato seran de tipo integer(int) y c será de tipo string(str) y el valor de salida será tipo string(str)
+    edad = a - b
+    return funcion2(c, edad)
+  
+if __name__ == "__main__":
+    print("Escribir una funcion que reciba un mensaje y un nombre  escriba en pantalla '<mensaje> <nombre>'")
+    print(funcion1("Hola", "Marco")) 
+    
+    
+    print("Escribir una funcion que reciba el nombre y la edad de una persona. EL mensaje de saluda tiene que ser: 'Hola <nombre> tienes <edad> años'")
+    print(funcion2("Marco", 20)) 
+    
+    
+    print("Escribir una funcion que recibe el año actual y el año de nacimiento, usando la funcion anterior enviando esta como argumento obtenga el mensaje: 'Hola <nombre> tienes <edad> años'")
+    print(funcion3(2022, 2002, "Marco"))
+    
