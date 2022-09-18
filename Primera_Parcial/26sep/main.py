de58909ce9631673935aa4cfc0cf4ec15445a153
Marco Antonio Ocampo <55845927+Mocampo28@users.noreply.github.com>
2022-09-18 18:53:55 -0500

    Create main.py

---
+++ Primera_Parcial/26sep/main.py
@@ -0,0 +1,20 @@
+# import carp.funk as s  #se importa la "libreria" funk para utilizar con la concatenacion "k" 
+
+
+
+def sumdos(a:int|float ,b:int|float):       #funcion que suma dos numeros
+   return a+b
+
+
+if __name__ == "__main__":       #inicio "main" del programa
+    
+    a=10
+     b=10
+
+     print(sumdos(a,b))
+    
+     print(s.sumar(a,b))
+     print(s.cuadrado(a))
+    
+    
+    
